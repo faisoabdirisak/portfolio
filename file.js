326e947d09ec1132cae29fab759905e180eb451b
faisoabdirisak <faisoabdirisak@gmail.com>
2021-11-07 18:13:29 +0300

    it will add javascript part of the priject

---
+++ file.js
@@ -0,0 +1,32 @@
+document.getElementById("graphy-btn").addEventListener("click", myFunction);
+function myFunction() {
+  window.location.href="https://faisoabdirisak.github.io/week1_id_project/";
+}
+
+document.getElementById("graphy1-btn").addEventListener("click", mFunction);
+function mFunction() {
+  window.location.href="https://faisoabdirisak.github.io/Desktop-Filter-Application-master";
+}
+
+document.getElementById("graphy2-btn").addEventListener("click", m2Function);
+function m2Function() {
+  window.location.href="https://faisoabdirisak.github.io/Week2_Id_Project/";
+}
+
+document.getElementById("graphy3-btn").addEventListener("click", m3Function);
+function m3Function() {
+  window.location.href="https://faisoabdirisak.github.io/BMI-Calculator/";
+}
+
+
+document.getElementById("graphy4-btn").addEventListener("click", m4Function);
+function m4Function() {
+  window.location.href="https://faisoabdirisak.github.io/week3_id_project/";
+}
+
+
+
+document.getElementById("graphy5-btn").addEventListener("click", m5Function);
+function m5Function() {
+  window.location.href="https://faisoabdirisak.github.io/week3_id_project/";
+}
